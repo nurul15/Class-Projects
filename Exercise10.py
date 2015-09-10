67b91736d898093861983bcadbb84e36603cf478
Nurul Amin <amin.nurul@hotmail.com>
2015-09-10 09:07:32 -0400

    my python files

---
+++ Exercise10.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 8, 2015
+
+@author: Test
+'''
+def main():
+    
+    tempc = float (input("Enter Temperature in CELSIUS: "))
+    tempf= ((tempc * 1.8) + 32)
+    print(tempc, "Celsius = ", tempf, "Farenhite")
+    
+    
+    
+    
+main()
