1a0d364252aaf50b79d3fb88969240fe387f922f
Nurul Amin <amin.nurul@hotmail.com>
2015-09-11 15:47:18 -0400

    Updated leap year

--- LeapYear.py
+++ LeapYear.py
@@ -8,7 +8,7 @@ def main():
     year= int(input("Type A Year: "))
     
     print("\n") #Prints on next line
-    if (year % 4 == 0) and (year % 100 == 0) or (year % 400 == 0) :
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0) :
         print("LEAP YEAR")
     else:
         print("NOT A LEAP YEAR")
